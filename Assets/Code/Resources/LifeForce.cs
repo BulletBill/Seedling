5f63c10d7339e2b56fb3e3c54edb9a1e0ba42e0c
Bill Caldwell <xiaolinslacker@gmail.com>
2023-10-29 23:27:14 -0500

    Start tilemap, stub important classes

---
+++ Assets/Code/Resources/LifeForce.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public partial class LifeForce : Node2D
+{
+    [Export] public String DisplayName;
+    [Export] public Texture2D IconLarge;
+    [Export] public Texture2D IconSmall;
+}
