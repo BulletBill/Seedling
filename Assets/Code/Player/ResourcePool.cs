f771963ff4097f0fe5447ce0be7e8e6c89d9aa39
Bill Caldwell <xiaolinslacker@gmail.com>
2023-11-01 16:46:20 -0500

    Temp images

--- Assets/Code/Player/ResourcePool.cs
+++ Assets/Code/Player/ResourcePool.cs
@@ -1,9 +1,12 @@
 using Godot;
 using System;
 
-public partial class LifeForce : Node2D
+public partial class Currency : Node2D
 {
     [Export] public String DisplayName;
     [Export] public Texture2D IconLarge;
     [Export] public Texture2D IconSmall;
+    [Export] public bool Consumed;
+    public int HeldAmount;
+    public int MaximumAmount;
 }
